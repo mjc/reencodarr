7c57704cd56a0d4a83102d3d284f6e11131ed6a3
Mika Cohen <mjc@kernel.org>
2024-11-26 15:40:45 -0700

    Add a DynamicSupervisor for the broadway pipelines

--- lib/reencodarr/pipeline.ex
+++ lib/reencodarr/pipeline.ex
@@ -1,9 +1,22 @@
 defmodule Reencodarr.Pipeline do
+  use DynamicSupervisor
+
   @moduledoc """
     Convenience methods for working with the pipelines that power Reencodarr.
   """
 
+  def start_link(_init_arg) do
+    DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @impl true
+  def init(:ok) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+
+  @spec start_video_scanner(any()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_video_scanner(path) do
-    Reencodarr.Pipeline.Scanner.Video.start_link(path: path)
+    spec = {Reencodarr.Pipeline.Scanner.Video, path: path}
+    DynamicSupervisor.start_child(__MODULE__, spec)
   end
 end
