787334d3148ec43e3267b00b8c7babc234775373
Michael J. Cohen <mjc@kernel.org>
2024-11-25 17:05:47 -0700

    Make path unlimited length

---
+++ priv/repo/migrations/20241126000141_make_path_longer.exs
@@ -0,0 +1,13 @@
+defmodule Reencodarr.Repo.Migrations.MakePathLonger do
+  use Ecto.Migration
+
+  def up do
+    alter table(:videos) do
+      modify :path, :text
+    end
+  end
+
+  def down do
+
+  end
+end
