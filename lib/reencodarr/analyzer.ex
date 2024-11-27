4c99b8516db9d20cd860cd8821ad5d4acf4563a0
Mika Cohen <mjc@kernel.org>
2024-11-27 14:36:44 -0700

    cleanup

--- lib/reencodarr/analyzer.ex
+++ lib/reencodarr/analyzer.ex
@@ -8,27 +8,28 @@ defmodule Reencodarr.Analyzer do
   end
 
   def init(_) do
+    {:ok, nil, {:continue, :subscribe}}
+  end
+
+  def handle_continue(:subscribe, state) do
     Phoenix.PubSub.subscribe(Reencodarr.PubSub, "video:found")
-    {:ok, nil}
+    {:noreply, state}
   end
 
   def handle_info(%{path: path, size: size, updated_at: updated_at}, state) do
-    Logger.info("Video file found: #{path}, size: #{size}, updated_at: #{updated_at}")
+    Logger.debug("Video file found: #{path}, size: #{size}, updated_at: #{updated_at}")
     case fetch_mediainfo(path) do
-      {:ok, mediainfo} ->
-        Media.upsert_video(%{path: path, size: size, mediainfo: mediainfo})
-      {:error, reason} ->
-        Logger.error("Failed to fetch mediainfo for #{path}: #{reason}")
+      {:ok, mediainfo} -> Media.upsert_video(%{path: path, size: size, mediainfo: mediainfo})
+      {:error, reason} -> Logger.error("Failed to fetch mediainfo for #{path}: #{reason}")
     end
     {:noreply, state}
   end
 
   defp fetch_mediainfo(path) do
-    case System.cmd("mediainfo", ["--Output=JSON", path]) do
-      {json, 0} ->
-        {:ok, Jason.decode!(json)}
-      {error, _exit_code} ->
-        {:error, error}
+    System.cmd("mediainfo", ["--Output=JSON", path])
+    |> case do
+      {json, 0} -> {:ok, Jason.decode!(json)}
+      {error, _} -> {:error, error}
     end
   end
 end
