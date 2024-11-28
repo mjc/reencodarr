f6cbb6f512b80a5cbd5acb3de9c2750c6e76780a
Mika Cohen <mjc@kernel.org>
2024-11-28 15:26:42 -0700

    Use a reasonable temp dir

--- lib/ab_av1.ex
+++ lib/ab_av1.ex
@@ -12,8 +12,12 @@ defmodule Reencodarr.AbAv1 do
   @spec crf_search(Reencodarr.Media.Video.t()) :: {any(), non_neg_integer()}
   def crf_search(video, vmaf_percent \\ 95) do
     rules = Rules.apply(video) |> Enum.map(fn {k, v} -> [to_string(k), to_string(v)] end) |> List.flatten()
-    args = ["crf-search", "-i", video.path, "--min-vmaf", to_string(vmaf_percent)] ++ rules
-    dbg(args)
+    args = [
+      "crf-search",
+      "-i", video.path,
+      "--min-vmaf", to_string(vmaf_percent),
+      "--temp-dir", temp_dir(),
+      ] ++ rules
     run_ab_av1(args)
   end
 
@@ -26,4 +30,7 @@ defmodule Reencodarr.AbAv1 do
     System.find_executable("ab-av1") || raise "ab-av1 not found"
   end
 
+  defp temp_dir() do
+    System.tmp_dir!() |> Path.join("ab-av1")
+  end
 end
