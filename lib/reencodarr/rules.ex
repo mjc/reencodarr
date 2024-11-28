f6d2bfe746586456eda1ea40d011b9404f34907a
Mika Cohen <mjc@kernel.org>
2024-11-28 14:55:36 -0700

    Fix keyword list usage

--- lib/reencodarr/rules.ex
+++ lib/reencodarr/rules.ex
@@ -26,9 +26,9 @@ defmodule Reencodarr.Rules do
 
   defp opus_arguments(channels) do
     [
-      {"--acodec", "libopus"},
-      {"--enc", "b:a=#{opus_bitrate(channels)}k"},
-      {"--enc", "ac=#{channels}"}
+      {:"--acodec", "libopus"},
+      {:"--enc", "b:a=#{opus_bitrate(channels)}k"},
+      {:"--enc", "ac=#{channels}"}
     ]
   end
 
@@ -42,13 +42,13 @@ defmodule Reencodarr.Rules do
   # TODO: detect CUDA capabilities
   @spec cuda(Media.Video.t()) :: keyword(String.t())
   def cuda(_) do
-    Keyword.new({"--enc-input", "hwaccel=cuda"})
+    Keyword.new([{:"--enc-input", "hwaccel=cuda"}])
   end
 
   # TODO: figure out how to detect grain or ask for that to be added to ab-av1
   @spec grain(Reencodarr.Media.Video.t(), integer) :: keyword(String.t())
   def grain(%Media.Video{hdr: hdr}, strength) when is_nil(hdr) do
-    Keyword.new({"--svt", "film-grain=#{strength}:film-grain-denoise=0"})
+    Keyword.new({:"--svt", "film-grain=#{strength}:film-grain-denoise=0"})
   end
 
   def grain(_), do: Keyword.new()
@@ -59,16 +59,16 @@ defmodule Reencodarr.Rules do
   """
   @spec hdr(Media.Video.t()) :: keyword(String.t())
   def hdr(%Media.Video{hdr: hdr}) when is_nil(hdr) do
-    Keyword.new([{"--svt", "tune=0"}])
+    Keyword.new([{:"--svt", "tune=0"}])
   end
 
   def hdr(_) do
-    Keyword.new([{"--encoder", "libx265"}, {"--preset", "medium"}])
+    Keyword.new([{:"--encoder", "libx265"}, {:"--preset", "medium"}])
   end
 
   @spec resolution(Reencodarr.Media.Video.t()) :: keyword(String.t())
   def resolution(%Media.Video{width: width}) when width > 1080 do
-    Keyword.new({"--vfilter", "scale=1920:-2"})
+    Keyword.new([{:"--vfilter", "scale=1920:-2"}])
   end
 
   def resolution(%Media.Video{width: width}) when width <= 1080 do
@@ -77,6 +77,6 @@ defmodule Reencodarr.Rules do
 
   @spec video(Reencodarr.Media.Video.t()) :: keyword(String.t())
   def video(%Media.Video{}) do
-    Keyword.new({"--pix-format", "yuv420p10le"})
+    Keyword.new([{:"--pix-format", "yuv420p10le"}])
   end
 end
