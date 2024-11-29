d8d861d92275a80ff81a66e894f69bc852b0675f
Mika Cohen <mjc@kernel.org>
2024-11-29 16:30:03 -0700

    a bit more cleanup

--- lib/reencodarr/ab_av1.ex
+++ lib/reencodarr/ab_av1.ex
@@ -1,22 +1,18 @@
 defmodule Reencodarr.AbAv1 do
   use GenServer
   alias Reencodarr.{Rules, Media}
+  alias Reencodarr.AbAv1.Helper
   require Logger
-
-  @crf_search_results ~r/
-    crf \s (?<crf>\d+) \s
-    VMAF \s (?<score>\d+\.\d+)
-    (?: \s predicted \s video \s stream \s size \s (?<size>[\d\.]+ \s \w+))?
-    \s \((?<percent>\d+)%\)
-    (?: \s taking \s (?<time>\d+ \s (?<unit>minutes|seconds|hours)))?
-    (?: \s predicted)?
-  /x
 
   @spec start_link(any()) :: GenServer.on_start()
   def start_link(_opts), do: GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
 
   @impl true
-  def init(:ok), do: {:ok, %{args: [], port: :none, video: :none, queue: :queue.new(), last_vmaf: nil}, {:continue, :resolve_ab_av1_path}}
+  def init(:ok),
+    do:
+      {:ok,
+       %{args: [], last_vmaf: :none, mode: :none, port: :none, queue: :queue.new(), video: :none},
+       {:continue, :resolve_ab_av1_path}}
 
   @impl true
   def handle_continue(:resolve_ab_av1_path, state) do
@@ -24,7 +20,7 @@ defmodule Reencodarr.AbAv1 do
     {:noreply, Map.put(state, :ab_av1_path, ab_av1_path)}
   end
 
-  @spec crf_search(Media.Video.t(), integer) :: :ok
+  @spec crf_search(Reencodarr.Media.Video.t()) :: :ok
   def crf_search(video, vmaf_percent \\ 95) do
     GenServer.cast(__MODULE__, {:crf_search, video, vmaf_percent})
   end
@@ -46,57 +42,100 @@ defmodule Reencodarr.AbAv1 do
   end
 
   @impl true
-  def handle_cast({:crf_search, video, vmaf_percent}, %{ab_av1_path: path, port: :none, queue: queue} = state) do
+  def handle_cast({:crf_search, video, vmaf_percent}, %{ab_av1_path: path, port: :none} = state) do
     Phoenix.PubSub.broadcast(Reencodarr.PubSub, "videos", %{action: "searching", video: video})
-    args = ["crf-search"] ++ build_args(video.path, vmaf_percent, build_rules(video))
+    args = ["crf-search"] ++ Helper.build_args(video.path, vmaf_percent, video)
 
-    port = Port.open({:spawn_executable, path}, [:binary, :exit_status, :line, :use_stdio, :stderr_to_stdout, args: args])
-    {:noreply, %{state | port: port, video: video, args: args, queue: queue}}
+    port =
+      Port.open({:spawn_executable, path}, [
+        :binary,
+        :exit_status,
+        :line,
+        :use_stdio,
+        :stderr_to_stdout,
+        args: args
+      ])
+
+    {:noreply, %{state | port: port, video: video, args: args, mode: :crf_search}}
   end
 
   @impl true
-  def handle_cast({:crf_search, video, vmaf_percent}, %{port: port, queue: queue} = state) when port != :none do
+  def handle_cast({:crf_search, video, vmaf_percent}, %{port: port} = state) when port != :none do
     Logger.info("Queueing crf search for video #{video.id}")
-    new_queue = :queue.in({:crf_search, video, vmaf_percent}, queue)
+    new_queue = :queue.in({:crf_search, video, vmaf_percent}, state.queue)
     {:noreply, %{state | queue: new_queue}}
   end
 
   @impl true
-  def handle_info({port, {:data, {:eol, data}}}, %{port: port} = state) do
-    output = String.split(data, "\n", trim: true)
-    result = attach_params(parse_crf_search(output), state.video, state.args)
+  def handle_info({port, {:data, {:eol, data}}}, %{port: port, mode: :crf_search} = state) do
+    result =
+      data
+      |> String.split("\n", trim: true)
+      |> Helper.parse_crf_search()
+      |> Helper.attach_params(state.video, state.args)
+
     Logger.info("Parsed output: #{inspect(result)}")
-    Phoenix.PubSub.broadcast(Reencodarr.PubSub, "videos", %{action: "crf_search_result", result: {:ok, result}})
+
+    Phoenix.PubSub.broadcast(Reencodarr.PubSub, "videos", %{
+      action: "crf_search_result",
+      result: {:ok, result}
+    })
+
     {:noreply, %{state | last_vmaf: List.last(result)}}
   end
 
   @impl true
-  def handle_info({port, {:exit_status, exit_code}}, %{port: port, queue: queue, last_vmaf: last_vmaf} = state) do
+  def handle_info(
+        {port, {:exit_status, exit_code}},
+        %{port: port, queue: queue, last_vmaf: last_vmaf, mode: :crf_search} = state
+      ) do
     result =
-      if exit_code in [0, 1],
-        do: {:ok, [Map.put(last_vmaf, "chosen", true)]},
-        else: {:error, "ab-av1 command failed with exit code #{exit_code}"}
+      if exit_code in [0, 1] do
+        {:ok, [Map.put(last_vmaf, "chosen", true)]}
+      else
+        {:error, "ab-av1 command failed with exit code #{exit_code}"}
+      end
 
     Logger.info("Exit status: #{inspect(result)}")
-    Phoenix.PubSub.broadcast(Reencodarr.PubSub, "videos", %{action: "crf_search_result", result: result})
+
+    Phoenix.PubSub.broadcast(Reencodarr.PubSub, "videos", %{
+      action: "crf_search_result",
+      result: result
+    })
 
     case :queue.out(queue) do
       {{:value, {:crf_search, video, vmaf_percent}}, new_queue} ->
         GenServer.cast(__MODULE__, {:crf_search, video, vmaf_percent})
-        {:noreply, %{state | port: :none, queue: new_queue, last_vmaf: nil}}
+        {:noreply, %{state | port: :none, queue: new_queue, last_vmaf: :none, mode: :none}}
+
       {:empty, _} ->
-        {:noreply, %{state | port: :none, last_vmaf: nil}}
+        {:noreply, %{state | port: :none, last_vmaf: :none, mode: :none}}
     end
   end
+end
 
-  defp attach_params(vmafs, video, args) do
+defmodule Reencodarr.AbAv1.Helper do
+  alias Reencodarr.Rules
+
+  @crf_search_results ~r/
+    crf \s (?<crf>\d+) \s
+    VMAF \s (?<score>\d+\.\d+)
+    (?: \s predicted \s video \s stream \s size \s (?<size>[\d\.]+ \s \w+))?
+    \s \((?<percent>\d+)%\)
+    (?: \s taking \s (?<time>\d+ \s (?<unit>minutes|seconds|hours)))?
+    (?: \s predicted)?
+  /x
+
+  def attach_params(vmafs, video, args) do
     filtered_args = remove_args(args, ["crf-search", "--min-vmaf", "--temp-dir"])
-    Enum.map(vmafs, &Map.put(&1, "video_id", video.id) |> Map.put("params", filtered_args))
+    Enum.map(vmafs, &(Map.put(&1, "video_id", video.id) |> Map.put("params", filtered_args)))
   end
 
-  defp remove_args(args, keys) do
+  def remove_args(args, keys) do
     Enum.reduce(args, {[], false}, fn
-      _arg, {acc, true} -> {acc, false}
+      _arg, {acc, true} ->
+        {acc, false}
+
       arg, {acc, false} ->
         if Enum.member?(keys, arg) do
           {acc, true}
@@ -108,18 +147,31 @@ defmodule Reencodarr.AbAv1 do
     |> Enum.reverse()
   end
 
-  defp build_rules(video) do
+  def build_rules(video) do
     Rules.apply(video)
     |> Enum.reject(fn {k, _v} -> k == :"--acodec" end)
     |> Enum.flat_map(fn {k, v} -> [to_string(k), to_string(v)] end)
   end
 
-  defp build_args(video_path, vmaf_percent, rules) do
-    ["-i", video_path, "--min-vmaf", Integer.to_string(vmaf_percent), "--temp-dir", temp_dir()] ++ rules
+  def build_args(video_path, vmaf_percent, video) do
+    rules = build_rules(video)
+
+    base_args = [
+      "-i",
+      video_path,
+      "--min-vmaf",
+      Integer.to_string(vmaf_percent),
+      "--temp-dir",
+      temp_dir()
+    ]
+
+    Enum.concat(base_args, rules)
   end
 
-  defp parse_crf_search(output) do
-    for line <- output, captures = Regex.named_captures(@crf_search_results, line), captures != nil do
+  def parse_crf_search(output) do
+    for line <- output,
+        captures = Regex.named_captures(@crf_search_results, line),
+        captures != nil do
       captures
       |> convert_time_to_duration()
       |> Enum.reject(fn {_, v} -> v in [nil, ""] end)
@@ -127,22 +179,27 @@ defmodule Reencodarr.AbAv1 do
     end
   end
 
-  defp convert_time_to_duration(%{"time" => time, "unit" => unit} = captures) do
+  def convert_time_to_duration(%{"time" => time, "unit" => unit} = captures) do
     case Integer.parse(time) do
-      {time_value, _} -> Map.put(captures, "time", convert_to_seconds(time_value, unit)) |> Map.delete("unit")
-      :error -> captures
+      {time_value, _} ->
+        Map.put(captures, "time", convert_to_seconds(time_value, unit)) |> Map.delete("unit")
+
+      :error ->
+        captures
     end
   end
 
-  defp convert_time_to_duration(captures), do: captures
+  def convert_time_to_duration(captures), do: captures
 
-  defp convert_to_seconds(time, "minutes"), do: time * 60
-  defp convert_to_seconds(time, "hours"), do: time * 3600
-  defp convert_to_seconds(time, _), do: time
+  def convert_to_seconds(time, "minutes"), do: time * 60
+  def convert_to_seconds(time, "hours"), do: time * 3600
+  def convert_to_seconds(time, _), do: time
 
-  defp temp_dir do
-    if function_exported?(Mix, :env, 0) and Mix.env() == :dev do
-      Path.join([File.cwd!(), "tmp", "ab-av1"])
+  def temp_dir do
+    cwd_temp_dir = Path.join([File.cwd!(), "tmp", "ab-av1"])
+
+    if File.exists?(cwd_temp_dir) do
+      cwd_temp_dir
     else
       Path.join(System.tmp_dir!(), "ab-av1")
     end
