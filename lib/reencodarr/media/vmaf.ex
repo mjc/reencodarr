30535e0d0692f2c8a5f2bda79cc835e61693f097
Mika Cohen <mjc@kernel.org>
2024-11-29 10:38:49 -0700

    convert time to an integer so we can compare it

--- lib/reencodarr/media/vmaf.ex
+++ lib/reencodarr/media/vmaf.ex
@@ -10,7 +10,7 @@ defmodule Reencodarr.Media.Vmaf do
     field :percent, :float
     field :chosen, :boolean, default: false
     field :size, :string
-    field :time, :string
+    field :time, :integer
     field :params, {:array, :string}
 
     belongs_to :video, Reencodarr.Media.Video
@@ -18,12 +18,19 @@ defmodule Reencodarr.Media.Vmaf do
     timestamps(type: :utc_datetime)
   end
 
+  @spec changeset(%{optional(:__struct__) => none(), optional(atom() | binary()) => any()}) ::
+          Ecto.Changeset.t()
   @doc false
   @spec changeset(t(), map()) :: Ecto.Changeset.t()
   def changeset(vmaf \\ %__MODULE__{}, attrs) do
     vmaf
     |> cast(attrs, [:score, :crf, :percent, :chosen, :size, :time, :params, :video_id])
+    |> maybe_mark_chosen()
     |> validate_required([:score, :crf, :params])
     |> foreign_key_constraint(:video_id, name: "vmafs_video_id_fkey")
+  end
+
+  defp maybe_mark_chosen(params) do
+    dbg(params)
   end
 end
---
+++ priv/repo/migrations/20241129173018_convert_time_to_seconds.exs
@@ -0,0 +1,44 @@
+defmodule Reencodarr.Repo.Migrations.ConvertTimeToSeconds do
+  use Ecto.Migration
+
+  def up do
+    alter table(:vmafs) do
+      add :duration, :integer
+    end
+
+    execute &convert_time_to_duration/0
+
+    alter table(:vmafs) do
+      remove :time
+    end
+
+    rename  table(:vmafs), :duration, to: :time
+
+  end
+
+
+  defp convert_time_to_duration do
+    execute """
+    UPDATE vmafs
+    SET duration =
+      CASE
+        WHEN time LIKE '% minutes' THEN CAST(SUBSTRING(time FROM '^[0-9]+') AS INTEGER) * 60
+        WHEN time LIKE '% hours' THEN CAST(SUBSTRING(time FROM '^[0-9]+') AS INTEGER) * 3600
+        WHEN time LIKE '% seconds' THEN CAST(SUBSTRING(time FROM '^[0-9]+') AS INTEGER)
+        ELSE NULL
+      END
+    """
+  end
+
+  defp convert_duration_to_time do
+    execute """
+    UPDATE vmafs
+    SET time =
+      CASE
+        WHEN duration % 3600 = 0 THEN (duration / 3600) || ' hours'
+        WHEN duration % 60 = 0 THEN (duration / 60) || ' minutes'
+        ELSE duration || ' seconds'
+      END
+    """
+  end
+end
