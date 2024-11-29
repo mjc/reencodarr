eb650a66758fad285a65ded3a67531728f513e65
Mika Cohen <mjc@kernel.org>
2024-11-29 10:16:27 -0700

    convenience alias

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,6 @@
 alias Reencodarr.AbAv1
 alias Reencodarr.Media
-alias Reencodarr.Media.{Video, Library}
+alias Reencodarr.Media.{Video, Library, Vmaf}
 alias Reencodarr.Repo
 alias Reencodarr.Rules
 alias Reencodarr.{Analyzer, Scanner}
