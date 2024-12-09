dc7dee92d5e0d77e12527564644d25d43b1c174e
Mika Cohen <mjc@kernel.org>
2024-12-09 11:11:21 -0700

    Use multi for faster inserting of videos

--- lib/reencodarr/sync.ex
+++ lib/reencodarr/sync.ex
@@ -3,6 +3,8 @@ defmodule Reencodarr.Sync do
   This module is responsible for syncing data between services and Reencodarr.
   """
   alias Reencodarr.{Media, Services}
+  alias Ecto.Multi
+  alias Reencodarr.Repo
   require Logger
 
   def sync_episode_files do
@@ -10,7 +12,7 @@ defmodule Reencodarr.Sync do
       {:ok, %Req.Response{body: shows}} ->
         shows
         |> Enum.map(& &1["id"])
-        |> Enum.flat_map(&fetch_episode_files/1)
+        |> Enum.map(&fetch_and_upsert_episode_files/1)
         |> List.flatten()
 
       {:error, reason} ->
@@ -18,10 +20,31 @@ defmodule Reencodarr.Sync do
     end
   end
 
-  defp fetch_episode_files(series_id) do
+  defp fetch_and_upsert_episode_files(series_id) do
     case Services.Sonarr.get_episode_files(series_id) do
       {:ok, %Req.Response{body: files}} ->
-        Enum.map(files, &upsert_video_from_episode_file/1)
+        multi =
+          Enum.reduce(files, Multi.new(), fn file, multi ->
+            attrs = %{
+              path: file["path"],
+              size: file["size"],
+              service_id: to_string(file["id"]),
+              service_type: "sonarr"
+            }
+
+            Multi.run(multi, "upsert_video_#{file["id"]}", fn _repo, _changes ->
+              Media.upsert_video(attrs)
+            end)
+          end)
+
+        case Repo.transaction(multi) do
+          {:ok, results} ->
+            Enum.map(results, fn {_, video} -> video end)
+
+          {:error, _failed_operation, failed_value, _changes} ->
+            Logger.error("Failed to upsert video: #{inspect(failed_value)}")
+            []
+        end
 
       {:error, _} ->
         []
@@ -33,7 +56,7 @@ defmodule Reencodarr.Sync do
       path: episode_file["path"],
       size: episode_file["size"],
       service_id: to_string(episode_file["id"]),
-      service_type: "sonarr"
+      service_type: :sonarr
     }
 
     case Media.upsert_video(attrs) do
@@ -46,9 +69,7 @@ defmodule Reencodarr.Sync do
     end
   end
 
-  def refresh_and_rename_from_video(%{service_type: :sonarr} = video) do
-    episode_file_id = video.service_id
-
+  def refresh_and_rename_from_video(%{service_type: :sonarr, service_id: episode_file_id} = video) do
     with {:ok, %Req.Response{body: episode_file}} <-
            Services.Sonarr.get_episode_file(episode_file_id),
          {:ok, _refresh_series} <- Services.Sonarr.refresh_series(episode_file["seriesId"]),
