1996a667541bae91bf40b28f2f53221fa1470e1e
Mika Cohen <mjc@kernel.org>
2024-12-09 10:01:05 -0700

    Add a function that gets all episode files for each series

--- lib/reencodarr/sync.ex
+++ lib/reencodarr/sync.ex
@@ -2,6 +2,25 @@ defmodule Reencodarr.Sync do
   @moduledoc """
   This module is responsible for syncing data between services and Reencodarr.
   """
-
   alias Reencodarr.Services
+
+  def get_all_episode_files do
+    case Services.Sonarr.get_shows() do
+      {:ok, %Req.Response{body: shows}} ->
+        shows
+        |> Enum.map(& &1["id"])
+        |> Enum.flat_map(&fetch_episode_files/1)
+        |> List.flatten()
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp fetch_episode_files(series_id) do
+    case Services.Sonarr.get_episode_files(series_id) do
+      {:ok, %Req.Response{body: files}} -> files |> dbg
+      {:error, _} -> []
+    end
+  end
 end
