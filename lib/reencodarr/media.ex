f7a3a9acb87e8e6eac6223abff7b0184d3164e25
Mika Cohen <mjc@kernel.org>
2024-12-09 17:07:50 -0700

    skip broadcasting updates to reencoded files for now

--- lib/reencodarr/media.ex
+++ lib/reencodarr/media.ex
@@ -124,10 +124,12 @@ defmodule Reencodarr.Media do
 
   @spec broadcast_change({:ok, Video.t()} | {:error, Ecto.Changeset.t()}) ::
           {:ok, Video.t()} | {:error, Ecto.Changeset.t()}
-  defp broadcast_change({:ok, video}) do
+  defp broadcast_change({:ok, %{reencoded: false} = video}) do
     ReencodarrWeb.Endpoint.broadcast("videos", "video:upsert", %{video: video})
     {:ok, video}
   end
+
+  defp broadcast_change({:ok, video}), do: {:ok, video}
 
   defp broadcast_change({:error, changeset}), do: {:error, changeset}
 
