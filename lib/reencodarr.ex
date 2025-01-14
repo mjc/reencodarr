7d00a0d2b64b3b82334493ec2d0caac24f2d0c56
Mika Cohen <mjc@kernel.org>
2025-01-13 20:17:17 -0700

    remove useless code

--- lib/reencodarr.ex
+++ lib/reencodarr.ex
@@ -6,22 +6,4 @@ defmodule Reencodarr do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "vmaf" do
-    field :crf, :integer
-    field :video_id, :integer
-    # ...existing code...
-  end
-
-  def changeset(vmaf, attrs) do
-    vmaf
-    |> cast(attrs, [:crf, :video_id])
-    |> validate_required([:crf, :video_id])
-    |> unique_constraint([:crf, :video_id])
-
-    # ...existing code...
-  end
 end
