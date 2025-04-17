1a7fb6143b2db9bfba3ecf930ec6da4938ffb6fa
Mika Cohen <mjc@kernel.org>
2025-04-17 10:18:51 -0600

    Add test for AbAv1 supervisor

---
+++ test/reencodarr/ab_av1_test.exs
@@ -0,0 +1,57 @@
+defmodule Reencodarr.AbAv1Test do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
+
+  alias Reencodarr.AbAv1
+
+  describe "start_link/1" do
+    test "starts the supervisor" do
+      assert {:ok, pid} = AbAv1.start_link([])
+      assert Process.alive?(pid)
+      assert Supervisor.which_children(pid) |> is_list()
+      Process.exit(pid, :normal)
+    end
+  end
+
+  describe "queue_length/0" do
+    test "returns queue lengths as integers" do
+      result = AbAv1.queue_length()
+      assert is_map(result)
+      assert Map.has_key?(result, :crf_searches)
+      assert Map.has_key?(result, :encodes)
+      assert is_integer(result.crf_searches)
+      assert is_integer(result.encodes)
+    end
+  end
+
+  describe "crf_search/2 and encode/1" do
+    test "calls GenServer.cast for crf_search and encode" do
+      # Patch GenServer.cast to capture calls
+      me = self()
+
+      patch = fn mod, msg ->
+        send(me, {:cast, mod, msg})
+        :ok
+      end
+
+      :meck.new(GenServer, [:passthrough])
+      :meck.expect(GenServer, :cast, patch)
+
+      video = %{id: 123, path: "foo", size: 100}
+      vmaf = %{id: 456, video_id: 123, crf: 28.0, score: 95.0, percent: 95.0}
+
+      AbAv1.crf_search(video, 95)
+      assert_receive {:cast, Reencodarr.AbAv1.CrfSearch, {:crf_search, ^video, 95}}
+
+      log =
+        capture_log(fn ->
+          AbAv1.encode(vmaf)
+        end)
+
+      assert_receive {:cast, Reencodarr.AbAv1.Encode, {:encode, ^vmaf}}
+      assert log =~ "Starting encode for VMAF"
+
+      :meck.unload()
+    end
+  end
+end
