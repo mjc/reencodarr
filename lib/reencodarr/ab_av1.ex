01fecd8b77744ede6e65ce87bc2fc3789dd70cbb
Mika Cohen <mjc@kernel.org>
2025-04-19 21:04:03 -0600

    Make AbAv1 supervisor more idiomatic

--- lib/reencodarr/ab_av1.ex
+++ lib/reencodarr/ab_av1.ex
@@ -1,21 +1,70 @@
 defmodule Reencodarr.AbAv1 do
   @moduledoc """
   Supervisor for AV1 CRF search and encode workers.
+
   Provides functions to queue CRF searches and encodes, and to check queue lengths.
   """
-  use Supervisor
 
+  use Supervisor
   require Logger
 
   alias Reencodarr.Media
 
+  ## Public API
+
   @doc """
-  Starts the AbAv1 supervisor.
+  Starts the `#{inspect(__MODULE__)}` supervisor.
   """
   @spec start_link(any()) :: Supervisor.on_start()
-  def start_link(_opts), do: Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  def start_link(_opts) do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
 
-  @impl true
+  @doc """
+  Returns the message queue lengths for the CRF search and encode GenServers.
+
+      iex> #{inspect(__MODULE__)}.queue_length()
+      %{crf_searches: 0, encodes: 0}
+  """
+  @spec queue_length() :: %{crf_searches: non_neg_integer(), encodes: non_neg_integer()}
+  def queue_length do
+    %{
+      crf_searches: queue_length_for(Reencodarr.AbAv1.CrfSearch),
+      encodes: queue_length_for(Reencodarr.AbAv1.Encode)
+    }
+  end
+
+  @doc """
+  Queues a CRF search for the given video and VMAF percent (default: 95).
+
+  ## Parameters
+
+    - `video`: a `%Media.Video{}` struct
+    - `vmaf_percent`: integer (default: 95)
+  """
+  @spec crf_search(Media.Video.t(), integer()) :: :ok
+  def crf_search(video, vmaf_percent \\ 95) do
+    GenServer.cast(Reencodarr.AbAv1.CrfSearch, {:crf_search, video, vmaf_percent})
+  end
+
+  @doc """
+  Queues an encode for the given VMAF result.
+
+  ## Parameters
+
+    - `vmaf`: a `%Media.Vmaf{}` struct
+  """
+  @spec encode(Media.Vmaf.t()) :: :ok
+  def encode(vmaf) do
+    Logger.debug("Queueing encode for VMAF", vmaf: vmaf)
+    GenServer.cast(Reencodarr.AbAv1.Encode, {:encode, vmaf})
+  end
+
+  ## Supervisor Callbacks
+
+  @behaviour Supervisor
+
+  @doc false
   def init(:ok) do
     children = [
       Reencodarr.AbAv1.CrfSearch,
@@ -25,40 +74,18 @@ defmodule Reencodarr.AbAv1 do
     Supervisor.init(children, strategy: :one_for_one)
   end
 
-  @doc """
-  Returns the message queue lengths for the CRF search and encode GenServers.
-  """
-  @spec queue_length() :: %{crf_searches: non_neg_integer(), encodes: non_neg_integer()}
-  def queue_length do
-    %{
-      crf_searches: queue_len(Reencodarr.AbAv1.CrfSearch),
-      encodes: queue_len(Reencodarr.AbAv1.Encode)
-    }
-  end
+  ## Private Helpers
 
-  defp queue_len(server) do
-    with pid when is_pid(pid) <- GenServer.whereis(server),
-         {:message_queue_len, len} <- Process.info(pid, :message_queue_len) do
-      len
-    else
+  @doc false
+  defp queue_length_for(server) do
+    case GenServer.whereis(server) do
+      pid when is_pid(pid) ->
+        case Process.info(pid, :message_queue_len) do
+          {:message_queue_len, len} -> len
+          _ -> 0
+        end
+
       _ -> 0
     end
-  end
-
-  @doc """
-  Queues a CRF search for the given video and VMAF percent (default: 95).
-  """
-  @spec crf_search(Media.Video.t(), integer()) :: :ok
-  def crf_search(video, vmaf_percent \\ 95) do
-    GenServer.cast(Reencodarr.AbAv1.CrfSearch, {:crf_search, video, vmaf_percent})
-  end
-
-  @doc """
-  Queues an encode for the given VMAF result.
-  """
-  @spec encode(Media.Vmaf.t()) :: :ok
-  def encode(vmaf) do
-    Logger.debug("Starting encode for VMAF: #{inspect(vmaf)}")
-    GenServer.cast(Reencodarr.AbAv1.Encode, {:encode, vmaf})
   end
 end
