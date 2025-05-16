092ed98ea58589b21ed325c6f96ed43e1b6b0e63
Mika Cohen <mjc@kernel.org>
2025-05-16 10:29:06 -0600

    Webhook seems to work for sonarr

--- lib/reencodarr_web/controllers/webhook_controller.ex
+++ lib/reencodarr_web/controllers/webhook_controller.ex
@@ -5,26 +5,30 @@ defmodule ReencodarrWeb.WebhookController do
   def sonarr(conn, %{"eventType" => "Test"} = params) do
     dbg(params)
     Logger.info("Received test event from Sonarr!")
-    send_resp(conn, 200, "ok")
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, %{"eventType" => "Grab", "release" => %{"releaseTitle" => title}} = _params) do
     Logger.info("Received grab event from Sonarr for #{title}!")
-    send_resp(conn, 200, "ok")
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, %{"eventType" => "Download", "episodeFiles" => episode_files} = params) do
     dbg(params)
-    Enum.each(episode_files, fn episode_file ->
+    results = Enum.map(episode_files, fn episode_file ->
       Logger.info("Received download event from Sonarr for episode #{episode_file["sceneName"]}!")
-      dbg(Reencodarr.Sync.upsert_video_from_file(episode_file, :sonarr))
+      Reencodarr.Sync.upsert_video_from_file(episode_file, :sonarr)
     end)
-    send_resp(conn, 200, "ok")
+    case Keyword.get_values(results, :ok) do
+      [] -> Logger.error("No successful upserts for download event")
+      _ -> Logger.info("Successfully processed download event for Sonarr")
+    end
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, %{"eventType" => "EpisodeFileDelete", "episodeFile" => episode_file} = _params) do
     Logger.info("Received delete event from Sonarr for episode file: #{inspect(episode_file)}")
-    send_resp(conn, 200, "ok")
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, %{"eventType" => "Rename", "renamedEpisodeFiles" => renamed_files} = _params) do
@@ -36,19 +40,19 @@ defmodule ReencodarrWeb.WebhookController do
       Reencodarr.Sync.upsert_video_from_file(file, :sonarr) |> dbg()
     end)
 
-    send_resp(conn, 200, "ok")
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, %{"eventType" => "EpisodeFile", "episodeFile" => episode_file} = params) do
     dbg(params)
     Logger.info("Received new episodefile event from Sonarr!")
     dbg(Reencodarr.Sync.upsert_video_from_file(episode_file, :sonarr))
-    send_resp(conn, 200, "ok")
+    send_resp(conn, :no_content, "")
   end
 
   def sonarr(conn, params) do
     dbg(params, label: "Received Sonarr webhook (other event)")
     Logger.info("Received unsupported event from Sonarr: #{inspect(params["eventType"])}")
-    send_resp(conn, 200, "ignored")
+    send_resp(conn, :no_content, "ignored")
   end
 end
