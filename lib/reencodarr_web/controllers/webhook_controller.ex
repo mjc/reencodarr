3813a939f9e2f17bb69853c09f3f79c89b772e09
Mika Cohen <mjc@kernel.org>
2025-05-16 09:47:59 -0600

    Webhook WIP

--- lib/reencodarr_web/controllers/webhook_controller.ex
+++ lib/reencodarr_web/controllers/webhook_controller.ex
@@ -2,22 +2,27 @@ defmodule ReencodarrWeb.WebhookController do
   use ReencodarrWeb, :controller
   require Logger
 
-  def sonarr(conn, %{"eventType" => "Test" = event_type} = params) do
+  def sonarr(conn, %{"eventType" => "Test"} = params) do
     dbg(params, label: "Received Sonarr webhook")
-    Logger.debug("Received test event from Sonarr!")
+    Logger.info("Received test event from Sonarr!")
+    send_resp(conn, 200, "ok")
+  end
+
+  def sonarr(conn, %{"eventType" => "Grab", "release" => %{"releaseTitle" => title}} = _params) do
+    Logger.info("Received grab event from Sonarr for #{title}!")
     send_resp(conn, 200, "ok")
   end
 
   def sonarr(conn, %{"eventType" => "EpisodeFile", "episodeFile" => episode_file} = params) do
     dbg(params, label: "Received Sonarr webhook")
-    Logger.debug("Received new episodefile event from Sonarr!")
+    Logger.info("Received new episodefile event from Sonarr!")
     dbg(Reencodarr.Sync.upsert_video_from_file(episode_file, :sonarr))
     send_resp(conn, 200, "ok")
   end
 
   def sonarr(conn, params) do
     dbg(params, label: "Received Sonarr webhook (other event)")
-    Logger.debug("Received unsupported event from Sonarr: #{inspect(params["eventType"])}")
+    Logger.info("Received unsupported event from Sonarr: #{inspect(params["eventType"])}")
     send_resp(conn, 200, "ignored")
   end
 end
