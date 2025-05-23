c91fbc25f4a75f8820748132f8a96dea66453e2f
Mika Cohen <mjc@kernel.org>
2025-05-23 11:28:50 -0600

    cleanup cleanup cleanup

--- lib/reencodarr/sync.ex
+++ lib/reencodarr/sync.ex
@@ -26,10 +26,12 @@ defmodule Reencodarr.Sync do
   require Logger
   alias Reencodarr.{Media, Services}
 
+  # Public API
   def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   def sync_episodes, do: GenServer.cast(__MODULE__, :sync_episodes)
   def sync_movies, do: GenServer.cast(__MODULE__, :sync_movies)
 
+  # GenServer Callbacks
   def init(state), do: {:ok, state}
 
   def handle_cast(:refresh_and_rename_series, state) do
@@ -38,61 +40,62 @@ defmodule Reencodarr.Sync do
   end
 
   def handle_cast(action, state) when action in [:sync_episodes, :sync_movies] do
-    {get_items, get_files, service_type} =
-      case action do
-        :sync_episodes -> {&Services.get_shows/0, &Services.get_episode_files/1, :sonarr}
-        :sync_movies -> {&Services.get_movies/0, &Services.get_movie_files/1, :radarr}
-      end
+    {get_items, get_files, service_type} = resolve_action(action)
 
     Phoenix.PubSub.broadcast(Reencodarr.PubSub, "progress", {:sync, :started})
 
     Task.start(fn ->
-      case get_items.() do
-        {:ok, %Req.Response{body: items}} when is_list(items) ->
-          items_count = length(items)
-          batch_size = 50
-
-          items
-          |> Stream.chunk_every(batch_size)
-          |> Stream.with_index()
-          |> Stream.each(fn {batch, batch_index} ->
-            batch
-            |> Task.async_stream(&fetch_and_upsert_files(&1["id"], get_files, service_type),
-              max_concurrency: 10,
-              timeout: 60_000,
-              on_timeout: :kill_task
-            )
-            |> Stream.with_index(batch_index * batch_size)
-            |> Stream.each(fn {res, idx} ->
-              progress = div((idx + 1) * 100, items_count)
-
-              Phoenix.PubSub.broadcast(
-                Reencodarr.PubSub,
-                "progress",
-                {:sync, :progress, progress}
-              )
-
-              if !match?({:ok, :ok}, res), do: Logger.error("Sync error: #{inspect(res)}")
-            end)
-            |> Stream.run()
-          end)
-          |> Stream.run()
-
-        _ ->
-          Logger.error("Sync error: unexpected response")
-      end
-
+      sync_items(get_items, get_files, service_type)
       Phoenix.PubSub.broadcast(Reencodarr.PubSub, "progress", {:sync, :complete})
     end)
 
     {:noreply, state}
   end
 
+  # Private Functions
+  defp resolve_action(:sync_episodes), do: {&Services.get_shows/0, &Services.get_episode_files/1, :sonarr}
+  defp resolve_action(:sync_movies), do: {&Services.get_movies/0, &Services.get_movie_files/1, :radarr}
+
+  defp sync_items(get_items, get_files, service_type) do
+    case get_items.() do
+      {:ok, %Req.Response{body: items}} when is_list(items) ->
+        process_items_in_batches(items, get_files, service_type)
+      _ ->
+        Logger.error("Sync error: unexpected response")
+    end
+  end
+
+  defp process_items_in_batches(items, get_files, service_type) do
+    items
+    |> Stream.chunk_every(50)
+    |> Stream.with_index()
+    |> Stream.each(&process_batch(&1, get_files, service_type, length(items)))
+    |> Stream.run()
+  end
+
+  defp process_batch({batch, batch_index}, get_files, service_type, total_items) do
+    batch
+    |> Task.async_stream(&fetch_and_upsert_files(&1["id"], get_files, service_type),
+      max_concurrency: 10,
+      timeout: 60_000,
+      on_timeout: :kill_task
+    )
+    |> Stream.with_index(batch_index * 50)
+    |> Stream.each(&handle_task_result(&1, total_items))
+    |> Stream.run()
+  end
+
+  defp handle_task_result({res, idx}, total_items) do
+    progress = div((idx + 1) * 100, total_items)
+    Phoenix.PubSub.broadcast(Reencodarr.PubSub, "progress", {:sync, :progress, progress})
+
+    if not match?({:ok, :ok}, res), do: Logger.error("Sync error: #{inspect(res)}")
+  end
+
   defp fetch_and_upsert_files(id, get_files, service_type) do
     case get_files.(id) do
       {:ok, %Req.Response{body: files}} when is_list(files) ->
         Enum.each(files, &upsert_video_from_file(&1, service_type))
-
       _ ->
         Logger.error("Fetch files error for id #{inspect(id)}")
     end
@@ -100,38 +103,67 @@ defmodule Reencodarr.Sync do
     :ok
   end
 
-  def upsert_video_from_file(file, service_type) do
-    info = Reencodarr.Media.MediaInfo.video_file_info_from_file(file, service_type)
-    if is_nil(info.size), do: Logger.warning("File size is missing: #{inspect(file)}")
-
-    if needs_analysis?(info),
-      do:
-        Reencodarr.Analyzer.process_path(%{
-          path: info.path,
-          service_id: info.service_id,
-          service_type: info.service_type
-        })
-
-    if !needs_analysis?(info) do
-      mediainfo = Reencodarr.Media.MediaInfo.from_video_file_info(info)
-
-      Media.upsert_video(%{
-        "path" => info.path,
-        "size" => info.size,
-        "service_id" => info.service_id,
-        "service_type" => info.service_type,
-        "mediainfo" => mediainfo,
-        "bitrate" => info.bitrate
-      })
-    end
-
-    :ok
+  def upsert_video_from_file(%Reencodarr.Media.VideoFileInfo{size: nil} = file, service_type) do
+    Logger.warning("File size is missing: #{inspect(file)}")
+    process_video_file(file, service_type)
   end
 
-  defp needs_analysis?(%{audio_codec: c, bitrate: b}) when c in ["TrueHD", "EAC3"] or b == 0,
-    do: true
+  def upsert_video_from_file(%Reencodarr.Media.VideoFileInfo{} = file, service_type) do
+    process_video_file(file, service_type)
+  end
 
-  defp needs_analysis?(_), do: false
+  def upsert_video_from_file(%{"path" => path, "size" => size, "mediaInfo" => media_info} = file, service_type) do
+    info = %Reencodarr.Media.VideoFileInfo{
+      path: path,
+      size: size,
+      service_id: file["id"],
+      service_type: service_type,
+      audio_codec: media_info["audioCodec"],
+      bitrate: file["bitrate"],
+      audio_channels: media_info["audioChannels"],
+      video_codec: media_info["videoCodec"],
+      resolution: "#{media_info["width"]}x#{media_info["height"]}",
+      video_fps: file["videoFps"],
+      video_dynamic_range: media_info["videoDynamicRange"],
+      video_dynamic_range_type: media_info["videoDynamicRangeType"],
+      audio_stream_count: length(media_info["audioLanguages"]),
+      overall_bitrate: file["overallBitrate"],
+      run_time: file["runTime"],
+      subtitles: media_info["subtitles"],
+      title: file["sceneName"]
+    }
+
+    upsert_video_from_file(info, service_type)
+  end
+
+  defp process_video_file(%Reencodarr.Media.VideoFileInfo{audio_codec: codec, bitrate: 0} = info, service_type) when codec in ["TrueHD", "EAC3"] do
+    Reencodarr.Analyzer.process_path(%{
+      path: info.path,
+      service_id: info.service_id,
+      service_type: service_type
+    })
+  end
+
+  defp process_video_file(%Reencodarr.Media.VideoFileInfo{resolution: resolution} = info, _service_type) do
+    resolution_tuple =
+      case String.split(resolution, "x") do
+        [width, height] -> {String.to_integer(width), String.to_integer(height)}
+        _ -> {0, 0} # Default to an invalid resolution if parsing fails
+      end
+
+    updated_info = %{info | resolution: resolution_tuple}
+
+    mediainfo = Reencodarr.Media.MediaInfo.from_video_file_info(updated_info)
+
+    Media.upsert_video(%{
+      "path" => updated_info.path,
+      "size" => updated_info.size,
+      "service_id" => updated_info.service_id,
+      "service_type" => updated_info.service_type,
+      "mediainfo" => mediainfo,
+      "bitrate" => updated_info.bitrate
+    })
+  end
 
   def refresh_operations(file_id, :sonarr) do
     with {:ok, %Req.Response{body: episode_file}} <- Services.Sonarr.get_episode_file(file_id),
@@ -146,7 +178,6 @@ defmodule Reencodarr.Sync do
   def refresh_operations(file_id, :radarr) do
     with {:ok, %Req.Response{body: movie_file}} <- Services.Radarr.get_movie_file(file_id),
          {:ok, _} <- Services.Radarr.refresh_movie(movie_file["movieId"]),
-         # Placeholder for rename
          {:ok, _} <- Services.Radarr.rename_movie_files(movie_file["movieId"]) do
       {:ok, "Refresh triggered for Radarr"}
     else
