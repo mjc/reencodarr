c0a530b8df090d76abf53bc0b284a5d8e9494546
Mika Cohen <mjc@kernel.org>
2025-05-25 11:46:50 -0600

    Bump pool size bc we do a lot of concurrency

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ config :reencodarr, Reencodarr.Repo,
   database: "reencodarr_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 50
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
