fe2252f3e6d13234a1e7f4f5c3b131de523398fa
Mika Cohen <mjc@kernel.org>
2025-06-01 10:35:23 -0600

    Don't show sync progress if not syncing

--- lib/reencodarr_web/live/components/sync_progress_component.ex
+++ lib/reencodarr_web/live/components/sync_progress_component.ex
@@ -7,19 +7,23 @@ defmodule ReencodarrWeb.SyncProgressComponent do
   def render(assigns) do
     ~H"""
     <div>
-      <div class="text-sm leading-5 text-gray-200 dark:text-gray-300 mb-1">Sync Progress</div>
-      <div class="flex items-center space-x-2">
-        <div class="w-full bg-gray-600 rounded-full h-2.5 dark:bg-gray-500">
-          <div
-            class="bg-indigo-600 h-2.5 rounded-full transition-all duration-300"
-            style={"width: #{if @sync_progress > 0, do: @sync_progress, else: 0}%"}
-          >
+      <%= if @sync_progress > 0 do %>
+        <div>
+          <div class="text-sm leading-5 text-gray-200 dark:text-gray-300 mb-1">Sync Progress</div>
+          <div class="flex items-center space-x-2">
+            <div class="w-full bg-gray-600 rounded-full h-2.5 dark:bg-gray-500">
+              <div
+                class="bg-indigo-600 h-2.5 rounded-full transition-all duration-300"
+                style="width: #{@sync_progress}%"
+              >
+              </div>
+            </div>
+            <div class="text-sm leading-5 text-gray-100 dark:text-gray-200 font-mono">
+              <strong>{@sync_progress}%</strong>
+            </div>
           </div>
         </div>
-        <div class="text-sm leading-5 text-gray-100 dark:text-gray-200 font-mono">
-          <strong>{@sync_progress}%</strong>
-        </div>
-      </div>
+      <% end %>
     </div>
     """
   end
