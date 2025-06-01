bb0826670a0f44c878eedb32ac0df064df32b58a
Mika Cohen <mjc@kernel.org>
2025-06-01 09:28:38 -0600

    Split the file lists because they were gross

---
+++ lib/reencodarr_web/live/components/encode_queue_component.ex
@@ -0,0 +1,49 @@
+defmodule ReencodarrWeb.EncodeQueueComponent do
+  use Phoenix.LiveComponent
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="bg-gray-900 rounded-lg shadow-lg p-6 border border-gray-700">
+      <h2 class="text-2xl font-bold text-indigo-500 mb-4">
+        CRF Search Queue
+      </h2>
+      <table class="table-auto w-full border-collapse border border-gray-700">
+        <thead>
+          <tr>
+            <th class="border border-gray-700 px-4 py-2 text-indigo-500">File Name</th>
+            <th class="border border-gray-700 px-4 py-2 text-indigo-500">Size</th>
+            <th class="border border-gray-700 px-4 py-2 text-indigo-500">Percent</th>
+          </tr>
+        </thead>
+        <tbody>
+          <%= for file <- @files do %>
+            <tr class="hover:bg-gray-800 transition-colors duration-200">
+              <td class="border border-gray-700 px-4 py-2 text-gray-300">
+                {format_name(file.video)}
+              </td>
+              <td class="border border-gray-700 px-4 py-2 text-gray-300">
+                {file.size}
+              </td>
+              <td class="border border-gray-700 px-4 py-2 text-gray-300">
+                {file.percent}
+              </td>
+            </tr>
+          <% end %>
+        </tbody>
+      </table>
+    </div>
+    """
+  end
+
+    defp format_name(%{path: path}) do
+    path = Path.basename(path)
+
+    case Regex.run(~r/^(.+?) - (S\d+E\d+)/, path) do
+      [_, series_name, episode_name] -> "#{series_name} - #{episode_name}"
+      [_, movie_name] -> movie_name
+      _ -> path
+    end
+  end
+
+end
