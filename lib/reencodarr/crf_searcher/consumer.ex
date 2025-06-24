e93a73a6bb7083d055a183f049c8a7d7e9dea0ed
Mika Cohen <mjc@kernel.org>
2025-06-24 11:40:46 -0600

    GenStage Pipelines seem to work now

--- lib/reencodarr/crf_searcher/consumer.ex
+++ lib/reencodarr/crf_searcher/consumer.ex
@@ -10,24 +10,40 @@ defmodule Reencodarr.CrfSearcher.Consumer do
 
   @impl true
   def init(:ok) do
-    {:consumer, :ok, subscribe_to: [{Reencodarr.CrfSearcher.Producer, max_demand: 5}]}
+    {:consumer, :ok, subscribe_to: [{Reencodarr.CrfSearcher.Producer, max_demand: 1}]}
   end
 
   @impl true
   def handle_events(videos, _from, state) do
     for video <- videos do
-      Task.Supervisor.start_child(Reencodarr.TaskSupervisor, fn ->
-        try do
-          Logger.info("Starting CRF search for #{video.path}")
-          AbAv1.crf_search(video)
-        rescue
-          e ->
-            Logger.error("CRF search failed for #{video.path}: #{inspect(e)}")
-            # Optionally mark video as failed or retry logic here
-        end
-      end)
+      try do
+        Logger.info("Starting CRF search for #{video.path}")
+        AbAv1.crf_search(video)
+
+        # Block until CRF search is actually complete
+        wait_for_crf_search_completion()
+
+        Logger.info("Completed CRF search for #{video.path}")
+      rescue
+        e ->
+          Logger.error("CRF search failed for #{video.path}: #{inspect(e)}")
+          # Optionally mark video as failed or retry logic here
+      end
     end
 
     {:noreply, [], state}
+  end
+
+  # Poll the CRF search status until it's no longer running
+  defp wait_for_crf_search_completion() do
+    case GenServer.call(Reencodarr.AbAv1.CrfSearch, :running?) do
+      :running ->
+        # Still running, wait a bit and check again
+        Process.sleep(100)
+        wait_for_crf_search_completion()
+      :not_running ->
+        # CRF search is complete
+        :ok
+    end
   end
 end
