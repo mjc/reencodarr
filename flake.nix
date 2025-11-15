53601c8113f597371cb1ee10e67d518d8e26a16c
Mika Cohen <mjc@kernel.org>
2025-11-15 11:06:37 -0700

    build: compile Erlang/OTP 28.1.1 from source
    Build Erlang 28.1.1 from source to get the latest bugfixes that
    may address segfault issues seen with earlier OTP 28 builds.

--- flake.nix
+++ flake.nix
@@ -14,8 +14,16 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
         lib = pkgs.lib;
-        # Use OTP 27 with Elixir 1.19 (OTP 28 has segfault issues)
-        erlang = pkgs.erlang_27;
+        # Build Erlang 28.1.1 from source
+        erlang = pkgs.beam.interpreters.erlang_28.overrideAttrs (oldAttrs: rec {
+          version = "28.1.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "erlang";
+            repo = "otp";
+            rev = "OTP-${version}";
+            hash = "sha256-2Yop9zpx3dY549NFsjBRghb5vw+SnUSEmv6VIA0m5yQ=";
+          };
+        });
         beamPackages = pkgs.beam.packagesWith erlang;
         elixir = beamPackages.elixir_1_19;
       in {
