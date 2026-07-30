1158ee193613295022d650243b0f104c5a1bc53a
Mika Cohen <mjc@kernel.org>
2026-07-30 11:59:46 -0600

    build(worker): update connection handling pin

--- nix/ab-av1-worker.nix
+++ nix/ab-av1-worker.nix
@@ -1,5 +1,5 @@
 {pkgs}: let
-  version = "0.11.4-worker-6164801";
+  version = "0.11.4-worker-b15575e";
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = "ab-av1";
@@ -8,8 +8,8 @@ in
     src = pkgs.fetchFromGitHub {
       owner = "mjc";
       repo = "ab-av1";
-      rev = "61648018dbea1931d404e6ce1014bd9db7deda71";
-      hash = "sha256-XKDhbbQa1JhGhLKTHONwOpnmw2xHos4NF5NBR7XBpBc=";
+      rev = "b15575e596e4296510dbc639256db82a0bd9f613";
+      hash = "sha256-mDJRIDwoS380wPPpiHVZPpIqtiNkHgo0irul49XphEQ=";
     };
 
     cargoHash = "sha256-QAIyQbUoLaZZd4K1PjIJodEZqGUbrcKnuCnitlNRVYA=";
