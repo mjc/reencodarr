383269e12f9de96b834fdc977732e544c1cf89f7
Mika Cohen <mjc@kernel.org>
2026-08-06 11:44:55 -0600

    build(worker): update ab-av1 worker

--- nix/ab-av1-worker.nix
+++ nix/ab-av1-worker.nix
@@ -1,5 +1,5 @@
 {pkgs}: let
-  version = "0.11.4-worker-b38ac4f";
+  version = "0.11.4-worker-f47659d";
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = "ab-av1";
@@ -8,8 +8,8 @@ in
     src = pkgs.fetchFromGitHub {
       owner = "mjc";
       repo = "ab-av1";
-      rev = "b38ac4f0618bd2057561a47fea068c7d94d31718";
-      hash = "sha256-JcLqBrFYS7JWKSQSwmQU9d8BLctIzf3RGE7LFV0o74Q=";
+      rev = "f47659d5276e0b30ad881f2a90447731884b30f6";
+      hash = "sha256-QZfLrkIAO1kwZFM+RgDIaPbpAKFinbPcONtAmqAH5YI=";
     };
 
     cargoHash = "sha256-QAIyQbUoLaZZd4K1PjIJodEZqGUbrcKnuCnitlNRVYA=";
