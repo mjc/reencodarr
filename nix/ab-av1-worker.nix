426fe13c47f8e4131e882fa523d2734847740e3e
Mika Cohen <mjc@kernel.org>
2026-08-24 13:06:46 -0600

    fix(worker): deploy process cleanup fix

--- nix/ab-av1-worker.nix
+++ nix/ab-av1-worker.nix
@@ -1,5 +1,5 @@
 {pkgs}: let
-  version = "0.11.4-worker-f47659d";
+  version = "0.11.4-worker-00b76a7";
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = "ab-av1";
@@ -8,11 +8,11 @@ in
     src = pkgs.fetchFromGitHub {
       owner = "mjc";
       repo = "ab-av1";
-      rev = "f47659d5276e0b30ad881f2a90447731884b30f6";
-      hash = "sha256-QZfLrkIAO1kwZFM+RgDIaPbpAKFinbPcONtAmqAH5YI=";
+      rev = "00b76a7";
+      hash = "sha256-WM+sKVqiCrO+19o4UrmzaFyTUy5llSfl+NjPg4X/4xY=";
     };
 
-    cargoHash = "sha256-QAIyQbUoLaZZd4K1PjIJodEZqGUbrcKnuCnitlNRVYA=";
+    cargoHash = "sha256-uqnxEfSuv0V5pimU7ciUwMqHshxPENtiiZf6Ol+KKds=";
 
     nativeBuildInputs = [pkgs.pkg-config];
     buildInputs = [pkgs.openssl];
