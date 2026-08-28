7f6c8f6e6b61519f9660b8ccc4b3f785c66e8a8d
Mika Cohen <mjc@kernel.org>
2026-08-27 19:31:57 -0600

    fix(worker): pin typed failure fix

--- nix/ab-av1-worker.nix
+++ nix/ab-av1-worker.nix
@@ -1,5 +1,5 @@
 {pkgs}: let
-  version = "0.11.5-worker-c0210f6";
+  version = "0.11.5-worker-a4a34a6";
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = "ab-av1";
@@ -8,8 +8,8 @@ in
     src = pkgs.fetchFromGitHub {
       owner = "mjc";
       repo = "ab-av1";
-      rev = "c0210f6";
-      hash = "sha256-ladsQFOVgkMVwrVrDK+6+jeFz9FN2mWzucn2oorGgTU=";
+      rev = "a4a34a6";
+      hash = "sha256-cPNFdlrOt8tCYXtvfzo8vrhFQ3snIphJ43bOzZH3fkw=";
     };
 
     cargoHash = "sha256-T+ejY4HbYMUoyLHK9WbDMe9xijcDNAOCMCoakJIfIW4=";
